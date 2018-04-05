d732ede7431a5a37f2f6c8d2fa9230f4bb8dc42a
HelenaZanini <helena.zanini1@gmail.com>
2018-04-05 17:01:13 -0300

    Falta atualizar os graficos

--- src/App.js
+++ src/App.js
@@ -7,89 +7,157 @@ import BarraHorinzontal from './components/barraHorizontal';
 import Rosca from './components/rosca';
 
 const API = "https://private-ae7eb-chart4.apiary-mock.com";
-let params = {empresas: ["todas"], tipo: ["Email", "ChatBot"], tempo: ["inicio"]}
+let params = { cliente: [], canal: [], tempo: [] }
 
 class App extends Component {
 
-  state = {
-    chamados: {
+  // constructor(props) {
+  //   super(props);
+  //   params = {
+  //     cliente: ["todas"],
+  //     canal: ["Email", "ChatBot"],
+  //     tempo: ["inicio"]
+  //   };
+  //   this.listaChamados(params);
+  //   this.state = {
+  //     data: {
+  //       email: {
+  //         resolvidos: 50,
+  //         não_resolvidos: 10
+  //       },
+  //       chatbot: {
+  //         resolvidos: 25,
+  //         não_resolvidos: 5
+  //       },
+  //       clientes: [
+  //         {
+  //           nome: 'nome',
+  //           valor: 0
+  //         }
+  //       ]
+  //     }
+  //   }
+  // }
+
+  // state = {
+  //   data: {
+  //     email: {
+  //       resolvidos: 50,
+  //       não_resolvidos: 10
+  //     },
+  //     chatbot: {
+  //       resolvidos: 25,
+  //       não_resolvidos: 5
+  //     },
+  //     clientes: [
+  //       {
+  //         nome: 'nome',
+  //         valor: 0
+  //       }
+  //     ]
+  //   }
+  // }
+
+  static defaultProps = {
+    data: {
       email: {
-        resolvidos: 50,
-         não_resolvidos: 10
-        },
+        resolvidos: 0,
+        não_resolvidos: 0
+      },
       chatbot: {
-        resolvidos: 25,
-         não_resolvidos: 5
-        },
+        resolvidos: 0,
+        não_resolvidos: 0
+      },
       clientes: [
         {
-        nome: 'nome',
-         valor: 0
+          nome: '',
+          valor: 0
         }
       ]
     }
   }
 
-  listaChamados = () => {
+  componentWillMount() {
+    params = {
+      cliente: ["todas"],
+      canal: ["Email", "ChatBot"],
+      tempo: ["inicio"]
+    };
+    this.listaChamados(params);
+
+  }
+
+  listaChamados = (dados) => {
     axios.get(`${API}/chamados`, {
       params: {
-        tipo: params.tipo,
-        tempo: params.tempo,
-        cliente: params.empresas
+        canal: dados.canal,
+        tempo: dados.tempo,
+        cliente: dados.cliente
       },
-      paramsSerializer: function(params) {
-        return Qs.stringify(params, {arrayFormat: 'repeat'})
+      paramsSerializer: function (params) {
+        return Qs.stringify(params, { arrayFormat: 'repeat' })
       },
     })
-    .then(response => {
-      console.log(response)
-       this.setState({
-        chamados: response.data,
+      .then(response => {
+        console.log(dados);
+        
+        this.props.data.email.resolvidos = response.data.email.resolvidos;
+        this.props.data.email.não_resolvidos = response.data.email.resolvidos;
+        this.props.data.chatbot.resolvidos = response.data.email.resolvidos;
+        this.props.data.chatbot.não_resolvidos = response.data.email.resolvidos;
+
+      })
+
+      .catch(error => {
       });
-    })
-    .catch(error => {
-    });
   }
-
-   
-
-/*  componentWillMount() {     
-    this.listaChamados();
-  }*/
 
   filtrarTodos = (ev) => {
-    console.log(ev);
-    ev.preventDefault();
 
-    this.listaChamados();
+    ev.preventDefault();
+    params = {
+      cliente: ["todas"],
+      canal: ["Email", "ChatBot"],
+      tempo: ["inicio"]
+    };
+
+    this.listaChamados(params);
   }
 
-  filtrar = () => {
-    
-    // Montar parametros da requisição
-    this.listaChamados();
+  filtrar = (ev) => {
+
+    ev.preventDefault();
+
+    // Montar parametros da requisição aqui
+
+    params = {
+      cliente: ["blob"],
+      canal: [],
+      tempo: []
+    };
+    console.log(params);
+    this.listaChamados(params);
   }
 
   render() {
-    
-    const { chamados } = this.state;
-    console.log(chamados)
-    const {filtrar, filtrarTodos} = this;
+    console.log(this.props.data);
+    const { filtrar, filtrarTodos } = this;
 
     return (
       <div className="App">
-        <Header filtrar={filtrar} filtrarTodos={filtrarTodos}/>
+        <Header filtrar={filtrar} filtrarTodos={filtrarTodos} />
         <div className="pacote">
 
           <div className='Graficos'>
-            <BarraHorinzontal chamados={chamados}/>
-            <Rosca chamados={chamados}/>
+            <BarraHorinzontal data={this.props.data} />
+            <Rosca data={this.props.data} />
           </div>
 
         </div>
       </div>
     );
   }
-}
 
+}
 export default App;
+
--- src/components/barraHorizontal.js
+++ src/components/barraHorizontal.js
@@ -1,63 +1,101 @@
 import React, { Component } from 'react';
+// import PropTypes from 'prop-types'
 import { HorizontalBar } from 'react-chartjs-2'
 
 
-const options = {
+// const options = {
 
-    responsive: true,
-    maintainAspectRatio: false
+//     responsive: true,
+//     maintainAspectRatio: false
 
-}
+// }
 
 
 class BarraHorizontal extends Component {
-    
-    constructor(props) {
-        super(props)
-        this.state = {
-            data: {
-                labels: ['Email', 'ChatBot-Freshdesk'],
-                datasets: [
-                    {
-                        label: 'Resolvidos',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
-                        borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
-                        data: [this.props.chamados.email.resolvidos, this.props.chamados.chatbot.resolvidos]
-                    },
-                    {
-                        label: 'Não-Resolvidos',
-                        backgroundColor: 'rgba(255,99,132,0.2)',
-                        borderColor: 'rgba(255,99,132,1)',
-                        borderWidth: 1,
-                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                        hoverBorderColor: 'rgba(255,99,132,1)',
-                        data: [this.props.chamados.email.não_resolvidos, this.props.chamados.chatbot.não_resolvidos]
-                    }
-                ]
-            }
-        }
 
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         responsive: true,
+    //         maintainAspectRatio: false
+    //     }
+    // this.state = {
+    // labels: ['Email', 'ChatBot-Freshdesk'],
+    // datasets: [
+    //     {
+    //         label: 'Resolvidos',
+    //         backgroundColor: 'rgba(255,99,132,0.2)',
+    //         borderColor: 'rgba(255,99,132,1)',
+    //         borderWidth: 1,
+    //         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+    //         hoverBorderColor: 'rgba(255,99,132,1)',
+    //         data: [this.props.data.email.resolvidos, this.props.data.chatbot.resolvidos]
+    //     },
+    //     {
+    //         label: 'Não-Resolvidos',
+    //         backgroundColor: 'rgba(255,99,132,0.2)',
+    //         borderColor: 'rgba(255,99,132,1)',
+    //         borderWidth: 1,
+    //         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+    //         hoverBorderColor: 'rgba(255,99,132,1)',
+    //         data: [this.props.data.email.não_resolvidos, this.props.data.chatbot.não_resolvidos]
+    //     }
+    // ]
+    //     }
+
+
+    // }
+    // componentWillMount() {
+    //     this.props.chartData.datasets[0].data = [this.props.data.email.resolvidos, this.props.data.chatbot.resolvidos];
+    //     this.props.chartData.datasets[1].data = [this.props.data.email.não_resolvidos, this.props.data.chatbot.não_resolvidos];
+    // }
+
+    static defaultProps = {
+        chartData: {
+            labels: ['Email', 'ChatBot-Freshdesk'],
+            datasets: [
+                {
+                    label: 'Resolvidos',
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [0, 0]
+                },
+                {
+                    label: 'Não-Resolvidos',
+                    backgroundColor: 'rgba(255,99,132,0.2)',
+                    borderColor: 'rgba(255,99,132,1)',
+                    borderWidth: 1,
+                    hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                    hoverBorderColor: 'rgba(255,99,132,1)',
+                    data: [0, 0]
+                }
+            ]
+        }
     }
-    
-    
-    
+
+    state = {
+        responsive: true,
+        maintainAspectRatio: false
+    }
+
+
 
     render() {
-        const { chamados } = this.props;
-        console.log(chamados);
-        console.log(this.props.chamados.email.resolvidos);
+        // const d = this.props.data;
+        this.props.chartData.datasets[0].data = [this.props.data.email.resolvidos, this.props.data.chatbot.resolvidos];
+        this.props.chartData.datasets[1].data = [this.props.data.email.não_resolvidos, this.props.data.chatbot.não_resolvidos];
         
-
-        
+        console.log(this.props.data);
+        console.log(this.props.data.email);
 
         return (
             <div>
-                <HorizontalBar data={this.state.data}
-                    options={options}
-                    width={500} height={250}/>
+                <HorizontalBar data={this.props.chartData}
+                    options={this.state}
+                    width={500} height={250} redraw />
             </div>
         );
     }
